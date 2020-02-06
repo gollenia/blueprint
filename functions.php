a73877bdf60061bc70d4a6076f8d6ce0ebd9569e
Thomas Gollenia <thomas@gollenia.at>
2020-02-06 10:38:47 +0100

    functions.php bearbeitet

--- functions.php
+++ functions.php
@@ -1,13 +1,12 @@
 <?php
 // Abhängigkeiten werden mittls PSR-4 Autoload geladen
-        require_once( __DIR__ . '/vendor/autoload.php' );
-        
-        // Timber ist ine Twig-Implementierung für Wordpress
-        $timber = new Timber\Timber();
-        
-        // Theme-Support und viele weitere Dinge können über die Config-Files erledigt werden
-        // Siehe config-Verzeichnis
-        $options = new Contexis\Core\Options;
-        
-        // Das Site-Objekt enthält alle Daten, die später an Twig übergeben werden
-        $site = new Contexis\Core\Site;
+require_once( __DIR__ . '/vendor/autoload.php' );
+
+// Timber ist ine Twig-Implementierung für Wordpress
+// Aber brauchen wir hier das Timber.Objekt überhaupt?
+$timber = new Timber\Timber();
+
+$options = new Contexis\Core\Options;
+
+// Das Site-Objekt enthält alle Daten, die später an Twig übergeben werden
+$site = new Contexis\Core\Site;
