7f12f320f136f823d1babd0fff796bac9878adf8
Thomas Gollenia <thomas@gollenia.at>
2020-04-07 10:19:10 +0200

    Added assets config

---
+++ config/assets.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Main Page Configuration
+ * 
+ * @since 1.0.0
+ */
+return [
+    "scripts" => [
+        [
+            'handle' => 'theme_script',
+            'url' => get_template_directory_uri() . '/assets/dist/main.js',
+            'dependencies' => [],
+            'version' => false,
+            'in_footer' => true
+        ],
+    ],
+    "styles" => [
+            [
+                'handle' => 'theme_style',
+                'url' => get_template_directory_uri() . '/assets/dist/app.css',
+                'dependencies' => [],
+                'version' => false,
+                'media' => 'all'
+            ]
+    ],
+    "remove_scripts" => [
+
+    ],
+    "remove_styles" => [
+        ['events-manager-css']
+    ]
+];
