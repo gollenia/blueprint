e9e3e3844fcc88d5f7da8ce552515538562b2149
Thomas Gollenia <thomas@gollenia.at>
2020-04-07 10:19:57 +0200

    Outsourced Theme Support

---
+++ config/theme_support.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    "menus" => [],
+    "post-thumbnails" => [],
+    "title-tag" => [],
+    'disable-custom-font-sizes' => [],
+    'disable-custom-colors' => [],
+    'wp-block-styles' => [],
+    'responsive-embeds' => [],
+    'editor-styles' => [],
+    'custom-background' => [],
+    'align-wide' => [],
+    "automatic-feed-links" => [],
+    "html5"=> ['comment-form', 'comment-list', 'gallery', 'caption'],
+    "post-formats"=> ['aside', 'image', 'video', 'quote', 'link', 'gallery', 'audio'],
+    "editor-color-palette" => [
+        [
+            "color" => "#eeeeee",
+            "name" => "Standard",
+            "slug" => "default"
+        ],
+        [
+            "color" => "#10bae7",
+            "name" => "Blau",
+            "slug" => "primary"
+        ],
+        [
+            "color" => "#f59b00",
+            "name" => "Orange",
+            "slug" => "warning"
+        ],
+        [
+            "color" => "#85bc22",
+            "name" => "Orange",
+            "slug" => "success"
+        ],
+        [
+            "color" => "#e3000b",
+            "name" => "Rot",
+            "slug" => "danger"
+        ],
+        [
+            "color" => "#fff",
+            "name" => "Weiß",
+            "slug" => "white"
+        ],
+        [
+            "color" => "#333",
+            "name" => "Schwarz",
+            "slug" => "black"
+        ]       	    
+    ]
+];
