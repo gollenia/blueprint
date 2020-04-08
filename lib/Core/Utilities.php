570ca7ca35fcadffdc38e4a75c2d8bc531819d1b
Thomas Gollenia <thomas@gollenia.at>
2020-04-08 09:19:33 +0200

    Debug-Function improved

--- lib/Core/Utilities.php
+++ lib/Core/Utilities.php
@@ -14,14 +14,27 @@ class Utilities {
      * 
      * @since 1.0.0
      * 
-     * @param $value array or variable to log
+     * @param array $value or variable to log
+     * @param bool $in_header put output into header to prevent multiple header-difinitions 
      * 
      */
-    public static function debug($value = "nothing to debug") {
-        if(!\wp_is_json_request()) {
+    public static function debug($value = "nothing to debug", $in_header = true) {
+
+        if( WP_DEBUG !== true || wp_is_json_request() ) { 
+            return; 
+        }
+
+        if (!$in_header) {
             echo "<script>console.log(" . json_encode($value) . ");</script>";
-        } 
+            return;
+        }
+
+        add_action ('wp_head', function() use (&$value) {
+            echo "<script>console.log(" . json_encode($value) . ");</script>";
+        });
+
     }
+    
 
     public static function render($context) {
         var_dump($context);
