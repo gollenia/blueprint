0f7d7bd51d4cdf0e8b0c89749b889bc3bea1e704
Thomas Gollenia <thomas@gollenia.at>
2020-04-08 09:20:33 +0200

    PHPDoc added

--- config/theme_support.php
+++ config/theme_support.php
@@ -1,5 +1,14 @@
 <?php
 
+/** 
+ * Add theme-support here. The can be registered with \Contexis\Wordpress\ThemeSupport:register()
+ * For further Info on options and Arguments see:
+ * 
+ * @link https://developer.wordpress.org/reference/functions/add_theme_support/
+ * 
+ * @return array Taxonomies
+*/
+
 return [
     "menus" => [],
     "post-thumbnails" => [],
