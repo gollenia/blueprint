b23d6f2d16f8317422e717c3a58465c6af02a3fc
Thomas Gollenia <thomas@gollenia.at>
2020-04-15 09:21:01 +0200

    New Post Controller

---
+++ lib/Controllers/Post.php
@@ -0,0 +1,27 @@
+<?php
+namespace Contexis\Controllers;
+
+use \Timber\URLHelper;
+use \Timber\Helper;
+use \Timber\User;
+
+/**
+ * Der Seiten-Controller erstellt einen PHP-Array (Context), in dem alle für den Aufbau der Seite benötigten
+ * Informationen gespeichert werden. 
+ * 
+ * @since 1.0.0
+ */
+
+class Post extends Page {
+
+    
+    public function __construct($site, $template = false) {
+        parent::__construct($site);
+        $this->context['author'] = get_the_author_meta( 'display_name', $this->context['post']->post_author );
+    }
+
+    protected function setTemplate() {
+        return array( 'pages/post.twig' );
+    }
+
+}
