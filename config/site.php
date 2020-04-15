119988bf868ec9ae58034aefe93f003bd2b4db34
Thomas Gollenia <thomas@gollenia.at>
2020-04-15 09:18:28 +0200

    deprecated

--- config/site.php
+++ config/site.php
@@ -1,72 +1,18 @@
 <?php
-#
-# Page Configuration
-#
-
-# Language for date and time calculation
+/**
+ * Main Page Configuration
+ * 
+ * @since 1.0.0
+ */
 return [
     "locale" => "de_AT", 
     "template_folder"=> ['templates', 'views'],
-    "theme_support" => [
-        "menus" => [],
-        "post-thumbnails"=> [],
-        "title-tag"=> [],
-        "align-wide"=>[],
-        "automatic-feed-links"=> [],
-        "html5"=> ['comment-form', 'comment-list', 'gallery', 'caption'],
-        "post-formats"=> ['aside', 'image', 'video', 'quote', 'link', 'gallery', 'audio'],
-        "editor-color-palette" => [
-            [
-                "color" => "#eeeeee",
-                "name" => "Standard",
-                "slug" => "default"
-            ],
-            [
-                "color" => "#10bae7",
-                "name" => "Blau",
-                "slug" => "primary"
-            ],
-            [
-                "color" => "#f59b00",
-                "name" => "Orange",
-                "slug" => "warning"
-            ],
-            [
-                "color" => "#85bc22",
-                "name" => "Orange",
-                "slug" => "success"
-            ],
-            [
-                "color" => "#e3000b",
-                "name" => "Rot",
-                "slug" => "danger"
-            ],
-            [
-                "color" => "#fff",
-                "name" => "Weiß",
-                "slug" => "white"
-            ],
-            [
-                "color" => "#333",
-                "name" => "Schwarz",
-                "slug" => "black"
-            ]
-            
-        ]
-    ],
-    "widgets" => [
-        'Footer-Bereich' => [
-            "id" => 'footer_area',
-            "description" => 'Widget-Bereich ganz unten auf der Seite',
-            "before_widget"=> '<div class="col col-12 col-lg-4">',
-            "after_widget"=> '</div>',
-            "before_title"=> '<h4>',
-            "after_title"=> '</h4>'
-        ]
-    ],
-    "mime" => [
-        'svg' => 'image/svg+xml',
-        'pdf' => 'application/pdf',
-        'zip' => 'application/zip'
-    ]
+
+    // wether or not config modules should be loaded
+    "acf" => true,
+    "taxonomies" => true,
+    "mimes" => true,
+    "theme_support" => true,
+    "widgets" => true,
+    "assets" => true
 ];
