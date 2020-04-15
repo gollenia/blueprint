923db2bb99f3a9d34f1b6138b3c97ca31f0b7b1e
Thomas Gollenia <thomas@gollenia.at>
2020-04-15 09:27:41 +0200

    render -> init as callback

--- lib/Wordpress/Shortcode.php
+++ lib/Wordpress/Shortcode.php
@@ -8,22 +8,29 @@ Class Shortcode {
     {
         add_shortcode( 
             $this->shortcodeName, 
-            [$this, "render"]
+            [$this, "init"]
         );
     }
 
     public static function register() {
+
         $files = scandir(get_template_directory() . '/lib/Wordpress/Shortcodes');
-		if (!$files) { return; }
+
+        if (!$files) { return; }
+        
 		foreach($files as $file) {
+
 			if ("php" === substr($file, -3)) {
-				require_once(get_template_directory() . '/lib/Wordpress/Shortcodes/' . $file);
-				$shortcode = 'Contexis\\Wordpress\\Shortcodes\\' . substr($file, 0, -4);
-				add_action( 'init', function() use(&$shortcode){
-                    new $shortcode;
-                });
+
+                require_once(get_template_directory() . '/lib/Wordpress/Shortcodes/' . $file);
+                
+                $shortcode = 'Contexis\\Wordpress\\Shortcodes\\' . substr($file, 0, -4);
+                add_action( 'init', function() use(&$shortcode){new $shortcode;});
+                
 			}
 		}
     }
+
+
     
 }
