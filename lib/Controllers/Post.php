be9fffb6448e636e7d684c11ba07080df82a57ef
Thomas Gollenia <thomas@gollenia.at>
2020-04-16 08:31:17 +0200

    Added Latest Posts

--- lib/Controllers/Post.php
+++ lib/Controllers/Post.php
@@ -15,9 +15,20 @@ use \Timber\User;
 class Post extends Page {
 
     
-    public function __construct($site, $template = false) {
+    public function __construct(\Contexis\Core\Site $site, $template = false) {
         parent::__construct($site);
         $this->context['author'] = get_the_author_meta( 'display_name', $this->context['post']->post_author );
+        $this->context['latest_posts'] = $this->getLatestPosts(5);
+    }
+
+    private function getLatestPosts(int $limit = 5) {
+        return new \Timber\PostQuery([
+            'post_type' => 'post',
+            'post__not_in' => [$this->context['post']->id],
+            'limit' => 5,
+            'orderby' => 'date',
+            'order' => 'DESC'
+        ]);
     }
 
     protected function setTemplate() {
