3b666e00193d928779f3bce1e3de8bad7cc1af8a
Thomas Gollenia <thomas@gollenia.at>
2020-04-16 08:38:08 +0200

    Last Build

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -6,10 +6,14 @@ import './../scss/app.scss';
 window.UIkit = UIkit;
 
 
-UIkit.util.on('#mobile-menu', 'show', function () {
-    document.getElementById('hamburger').classList.add("is-active");
+UIkit.util.on('#mobile-menu', 'beforeshow', function (event) {
+    if(event.target.id == "mobile-menu") {
+        document.getElementById('hamburger').classList.add("is-active");
+    }
 });
 
-UIkit.util.on('#mobile-menu', 'hide', function () {
-    document.getElementById('hamburger').classList.remove("is-active");
+UIkit.util.on('#mobile-menu', 'beforehide', function (event) {
+    if(event.target.id == "mobile-menu") {
+        document.getElementById('hamburger').classList.remove("is-active");
+    }
 });
