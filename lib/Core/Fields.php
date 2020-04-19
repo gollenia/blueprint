068b52535218108d6e5279b29e29be435e5d88b5
Thomas Gollenia <thomas@gollenia.at>
2020-04-19 15:38:04 +0200

    Option-Pages Support added

--- lib/Core/Fields.php
+++ lib/Core/Fields.php
@@ -11,15 +11,28 @@ namespace Contexis\Core;
 
 Class Fields {
 
-    public static function register($fields) {
+    public static function register($options) {
+      
+      if ($options === null) { return; }
+      if ($options['fields'] !== null) { self::registerFields($options['fields']); }   
+      if ($options['pages'] !== null) { self::registerPages($options['pages']); }   
+		
+    }
 
-		if ($fields === null) { return; }
-        
-		add_action('acf/init', function() use (&$fields){
-			foreach ($fields as $field) {
-                acf_add_local_field_group($field);	
-            }	
-		});
+    private static function registerFields($fields) {
+      add_action('acf/init', function() use (&$fields){
+        foreach ($fields as $field) {
+                  acf_add_local_field_group($field);	
+              }	
+      });
+    }
+
+    private static function registerPages($pages) {
+      add_action('acf/init', function() use (&$pages){
+        foreach ($pages as $page) {
+          acf_add_options_page($page);	
+              }	
+      });
     }
     
 }
