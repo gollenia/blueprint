cce23e2481c9faa1e1bad7fae1ee0c622b82d7d9
Thomas Gollenia <thomas@gollenia.at>
2020-04-29 09:01:05 +0200

    in the End, tnothing changed here...

--- functions.php
+++ functions.php
@@ -21,34 +21,4 @@ $site = new Contexis\Core\Site($config);
 
 
 
-add_action("emp_forms_output_field", "print_field");
 
-function print_field($field) {
-    if ($field=="") {return;}
-    $html = new \Wa72\HtmlPageDom\HtmlPageCrawler($field);
-    $input = $html->filter('input');
-    $label = $html->filter('label');
-    $select = $html->filter('select');
-    $textarea = $html->filter('textarea');
-
-    if($html->filter('select')->count()) {
-        $select->addClass("uk-select");
-    }
-
-    if($html->filter('textarea')->count()) {
-        $select->addClass("uk-textarea");
-    }
-    
-    if($html->filter('input')->count()) {
-        switch ($input->getAttribute('type')) {
-            case "text":
-                $input->addClass("uk-input");
-                break;
-            default: 
-                break;
-        }
-    }
-
-    echo $html->saveHTML();
-    
-}
