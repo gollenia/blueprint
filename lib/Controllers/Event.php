a80653468c09060adf69c0c9b5dce3671f6dfccf
Thomas Gollenia <thomas@gollenia.at>
2020-04-29 11:52:52 +0200

    Booking form vor Events Manager tuning

--- lib/Controllers/Event.php
+++ lib/Controllers/Event.php
@@ -17,8 +17,110 @@ class Event extends \Contexis\Core\Controller {
     
     public function __construct($site, $template = false) {
         parent::__construct($site);
-        $this->setTemplate('pages/event.twig');
+        $this->setTemplate('pages/event.twig'); 
+        $this->context['booking'] = $this->getBookingForm();
     }
 
+    private function getBookingForm() {
+        $post = \Timber\Timber::get_post();
+        $content = apply_filters( 'the_content', $post->content );
+        return $this->filterForm($content);
+    }
+
+    public function filterForm($form) {
+        if ($form=="") {return;}
+        $html = new \Wa72\HtmlPageDom\HtmlPageCrawler($form);
+    
+        
+        if($html->filter(".input-country")->count()) {
+            $html->filter("option[value=0]")->setAttribute("disabled", "")->setAttribute("value", "")->setText("Land auswählen...");
+        }
+
+        // Add UiKit Classes
+        
+        if($html->filter('input')->count()) {
+            foreach ($html->filter('input') as $field) {
+                $field = \Wa72\HtmlPageDom\HtmlPageCrawler::create($field);
+                
+                switch ($field->getAttribute('type')) {
+                    case "text":
+                        $field->addClass("uk-input");
+                        break;
+                    case "password":
+                        $field->addClass("uk-input");
+                        break;
+                    case "checkbox":
+                        $field->addClass("uk-checkbox");
+                        break;
+                    case "submit":
+                        $field->addClass("uk-button");
+                        break;
+                    default:       
+                }
+
+            }
+        }
+
+        if($html->filter('textarea')->count()) {
+            $html->filter('textarea')->addClass("uk-textarea");
+        }
+
+        if($html->filter('select')->count()) {
+            $html->filter('select')->addClass("uk-select");
+        }
+
+        if($html->filter('table')->count()) {
+            $html->filter('table')->wrap("<div>", "</div>");
+        }
+        if($html->filter('.em-booking-form-details')->count()) {
+            $html->filter('.em-booking-form-details')->wrap("<div>", "</div>");
+        }
+
+        // Add some infos for form-validation
+
+        if($html->filter('.input-user-field')->count()) {
+            $fields = $html->filter('.input-user-field,.input-group');
+            foreach ($fields as $field) {
+                $field = \Wa72\HtmlPageDom\HtmlPageCrawler::create($field);
+                if($field->filter('span.em-form-required')->count() && $field->filter('input,select')->count()) {
+                    $field->filter('span.em-form-required')->remove();
+                    $field->filter('select')->addClass('required')->setAttribute("required", true);
+                    $field->filter('input')->addClass('required')->setAttribute("required", true);
+
+                }
+                if($field->filter('label')->count() && $field->filter('input[type="text"]')->count()) {
+                    $placeholder = $field->filter('label')->text();
+                    $field->filter('label')->remove();
+                    $field->filter('input')->setAttribute('placeholder', $placeholder);
+                }
+
+                if($field->filter('label')->count() && $field->filter('select')->count()) {
+                    $field->filter('label')->remove();
+                    $field->filter('input')->setAttribute('placeholder', $placeholder);
+                }
+            }
+        }
+
+        $types = [
+            "#user_email" => "email",
+            "#dbem_phone" => "tel",
+            "#dbem_zip" => "number",
+        ];
+
+        foreach($types as $id => $type) {
+            if($html->filter($id)->count()) {
+                $html->filter($id)->setAttribute("type", $type);
+            }
+        }
+
+        if($html->filter("form")->count()) {
+            $html->filter("form")->setAttribute("uk-grid", true);
+            $html->filter("form")->setAttribute("novalidate", true);
+            $html->filter("form")->addClass("uk-child-width-1-2@m");
+        }
+
+        return $html->saveHTML();
+         
+    }
 
 }
