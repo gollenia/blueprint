d93c36cfedeadc08655064b9021be4b9bb482408
Thomas Gollenia <thomas@gollenia.at>
2020-07-20 11:52:19 +0200

    new Controller

---
+++ lib/Controllers/Categories.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Contexis\Controllers;
+
+/**
+ * Der Seiten-Controller erstellt einen PHP-Array (Context), in dem alle für den Aufbau der Seite benötigten
+ * Informationen gespeichert werden. 
+ * 
+ * @since 1.0.0
+ */
+
+class Categories extends \Contexis\Core\Controller{
+
+    public function __construct($site, $template = false) {
+
+        parent::__construct($site);
+        $this->addToContext([
+            'posts' => new \Timber\PostQuery(),
+            'category' => single_cat_title('', false)
+        ]);
+        \Contexis\Core\Utilities::debug($this->get_context('category'));
+        $this->setTemplate('pages/categories.twig');
+        
+    }
+    
+}
