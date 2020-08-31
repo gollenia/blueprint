b55784c9355d18e6f348607476ec2d497744c814
Thomas Gollenia <thomas@gollenia.at>
2020-08-31 07:27:11 +0200

    close modal if present

--- assets/src/js/contact/contactForm.js
+++ assets/src/js/contact/contactForm.js
@@ -1,16 +1,19 @@
 import UIkit from 'uikit';
 
 export default (function() {
+    
     document.addEventListener("wpcf7invalid ", function(event) {
-        UIkit.notification(event.detail.apiResponse.message, {status:'danger'});
+        UIkit.notification(event.detail.apiResponse.message, {status:'danger', timeout: 7000});
     });
     document.addEventListener("wpcf7spam", function(event) {
-        UIkit.notification(event.detail.apiResponse.message, {status:'danger'});
+        UIkit.notification(event.detail.apiResponse.message, {status:'danger', timeout: 7000});
     });
     document.addEventListener("wpcf7mailsent", function(event) {
-        UIkit.notification(event.detail.apiResponse.message, {status:'success'});
+        UIkit.modal(".uk-modal").hide();
+        UIkit.notification(event.detail.apiResponse.message, {status:'success', timeout: 7000});
     });
     document.addEventListener("wpcf7mailfailed", function(event) {
-        UIkit.notification(event.detail.apiResponse.message, {status:'danger'});
+        UIkit.modal(".uk-modal").hide();
+        UIkit.notification(event.detail.apiResponse.message, {status:'danger', timeout: 7000});
     });
 })();
