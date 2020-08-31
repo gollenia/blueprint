98bbb8bfd63ecc6359bf12c210dbdd3201752e99
Thomas Gollenia <thomas@gollenia.at>
2020-08-31 07:31:47 +0200

    moved add_required_to_wpcg7 to this Class

--- lib/Wordpress/Plugins/ContactForm7.php
+++ lib/Wordpress/Plugins/ContactForm7.php
@@ -11,7 +11,7 @@ namespace Contexis\Wordpress\Plugins;
  */
 Class ContactForm7 {
 
-    public static function addCustiomAttribute($attribute) {
+    public static function add_custom_attribute($attribute) {
 
         add_filter( 'shortcode_atts_wpcf7', function($out, $pairs, $atts) use (&$attribute) {
 	
@@ -24,5 +24,17 @@ Class ContactForm7 {
         }, 10, 3 );
 		
     }
+
+    /**
+	 * Hack for Contact form 7 to add "required" attribute, will hopefully becom
+	 * deprecated in future
+	 * 
+	 * @since 1.0.0
+	 */
+	public static function add_required_to_wpcf7() {
+		add_filter( 'wpcf7_form_elements', function($content) {
+			return str_replace('aria-required="true"', 'required aria-required="true"', $content);
+		});
+	}
     
 }
