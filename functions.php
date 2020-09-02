dde280f2a75aa80f9adc99867f19dc1a91ead81f
Thomas Gollenia <thomas@gollenia.at>
2020-09-02 15:43:08 +0200

    :bug: Config must be loaded in index.php for routing

--- functions.php
+++ functions.php
@@ -29,10 +29,3 @@ if(!get_transient( $_SERVER['REMOTE_ADDR'])) {
  */
 require_once( __DIR__ . '/vendor/autoload.php' );
 
-// either set choose config files manually or scan whole directory
-// $config_array = ["site", "theme_support", "fields", "mimes", "assets", "routes", "widgets"];
-
-$config = new \Contexis\Core\Config(get_template_directory() . "/config/");
-
-// the site-object stores information about the whole site. It is later passed to twig.
-$site = new Contexis\Core\Site($config);
