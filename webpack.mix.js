492e0644b1112fc6144aaf1b59ffc8002ff4ff3e
Thomas Gollenia <thomas@gollenia.at>
2020-11-23 07:58:34 +0100

    app.js and admin.css added

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,10 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
-mix.postCss('./assets/src/css/style.css', './assets/css/style.css',
+mix.postCss('./assets/src/css/style.css', './assets/dist/style.css',
 	tailwindcss('./tailwind.config.js')
 );
+
+mix.postCss('./assets/src/css/admin.css', './assets/dist/admin.css');
+
+mix.js('./assets/src/js/app.js', './assets/dist/app.js');
