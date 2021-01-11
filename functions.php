6d53d7a8c26452d19cddffbbe71e5ca8caf09622
Thomas Gollenia <thomas@gollenia.at>
2021-01-11 14:43:34 +0100

    Redirection removed for production Site

--- functions.php
+++ functions.php
@@ -1,26 +1,6 @@
 <?php
 
 
-// BETA Code. This Code will be removed, when Site is complete
-
-// Debug-Hack. Kommt später weg
-$url =  'http://' . $_SERVER['SERVER_NAME'];
-$url = explode(".", parse_url($url, PHP_URL_HOST));
-$url = end($url);
-if (isset($_GET['dev']) ) {
-    set_transient( $_SERVER['REMOTE_ADDR'], "yes", 60*60*12 );
-}
-
-if($url === "ch") {
-    $url = "ch-de";
-}
-
-if(!get_transient( $_SERVER['REMOTE_ADDR'])) {
-    header("Location: http://www.kids-team.com/" . $url);
-    exit();
-}
-
-
 
 /**
  * First we need to load the Composer Autoload which is responsinble for making Classes available
