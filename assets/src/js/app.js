33914a22b1e678b01d52673e4d849262b9633181
Thomas Gollenia <thomas@gollenia.at>
2021-01-21 10:30:49 +0100

    manualinvalid event, obsolete

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,5 +1,8 @@
 import validateInput from "./validity.js";
 import  './booking.js';
 
+const event = new Event('manualvalid');
+
 document.addEventListener("focusout", validateInput);
 document.addEventListener("change", validateInput);
+
