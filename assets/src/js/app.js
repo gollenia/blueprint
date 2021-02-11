367b522043688427c93d3a5a6cb82fb9bb5b2ce2
Thomas Gollenia <thomas@gollenia.at>
2021-02-11 11:09:13 +0100

    New Build

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,7 +1,7 @@
 import validateInput from "./validity.js";
 import  './booking.js';
 
-const event = new Event('manualvalid');
+
 
 document.addEventListener("focusout", validateInput);
 document.addEventListener("change", validateInput);
