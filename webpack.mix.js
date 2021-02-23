ae4af84f6320ef0098cbec1eb3cff52345d93ead
Thomas Gollenia <thomas@gollenia.at>
2021-02-23 15:26:59 +0100

    New require

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,9 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
-mix.postCss('./assets/src/css/style.css', './assets/dist/style.css',
-	tailwindcss('./tailwind.config.js')
-);
+mix.postCss('./assets/src/css/style.css', './assets/dist/style.css', [
+	require("tailwindcss"),
+]);
 
 mix.postCss('./assets/src/css/admin.css', './assets/dist/admin.css');
 
