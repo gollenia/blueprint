bdfb557e9fec15c7b855f6ea77e50495567b9c56
Thomas Gollenia <thomas@gollenia.at>
2021-06-22 08:50:26 +0200

    old event taxonomies removed

--- config/taxonomies.php
+++ config/taxonomies.php
@@ -26,47 +26,10 @@ return [
             'show_in_rest' => true,
             'description' => "Für wen ist der Beitrag geeignet?",
             'show_admin_column' => true,
-            'query_var'         => true,
+            'query_var'         => true, 
             'rewrite' => ['slug' => 'audience']
         ]
     ],
-    "event_category" => [
-        "object_type" => EM_POST_TYPE_EVENT,
-        "options" => [
-            'hierarchical' => true,
-            'labels' => [
-                'name' => "Veranstaltungsart",
-                'singular_name' => "Veranstaltungsart",
-                'all_items' => "Alle Veranstaltungsarten",
-                'add_new_item' => "Neue Veranstaltungsart erstellen",
-                'not_found' => "Keine Veranstaltungsart gefunden",
-            ],
-            'show_ui' => true,
-            'show_in_rest' => true,
-            'description' => "Art der Veranstaltung",
-            'show_admin_column' => true,
-            'query_var'         => true,
-            'rewrite' => ['slug' => 'event_category']
-        ]
-    ],
-    "event_tag" => [
-        "object_type" => EM_POST_TYPE_EVENT,
-        "options" => [
-            'hierarchical' => false,
-            'labels' => [
-                'name' => "Schlagwort",
-                'singular_name' => "Schlagworte",
-                'all_items' => "Alle Schlagworte",
-                'add_new_item' => "Neues Schlagwort erstellen",
-                'not_found' => "Keine Schlagworte gefunden",
-            ],
-            'show_ui' => true,
-            'show_in_rest' => true,
-            'description' => "Schlagworte, um Veranstaltungen besser zu kategorisieren",
-            'show_admin_column' => true,
-            'query_var'         => true,
-            'rewrite' => ['slug' => 'event_tag']
-        ]
-    ]
+    
     
 ];
