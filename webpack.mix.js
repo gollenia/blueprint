d2b54e47b8ecc1b6059963d303d754b7d26dd89e
Thomas Gollenia <thomas@gollenia.at>
2021-09-23 09:44:43 +0200

    styles updated

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,3 +8,5 @@ mix.postCss('./assets/src/css/style.css', './assets/dist/style.css', [
 mix.postCss('./assets/src/css/admin.css', './assets/dist/admin.css');
 
 mix.js('./assets/src/js/app.js', './assets/dist/app.js');
+
+mix.js('./assets/src/js/admin.js', './assets/dist/admin.js');
