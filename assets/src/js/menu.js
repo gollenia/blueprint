1dd33140d72483e52ae39d9e4e88e915161f9cce
Thomas Gollenia <thomas@gollenia.at>
2021-11-02 10:17:06 +0100

    menu script

---
+++ assets/src/js/menu.js
@@ -0,0 +1,32 @@
+const menu = document.querySelectorAll('.menu__item--has-dropdown')
+if(menu.length > 0) {
+    menu.forEach( (element) => {
+        element.addEventListener('click', (event) => {
+            event.stopPropagation();
+            var open = false;
+            if(event.currentTarget.classList.contains("menu__item--open")) {
+                open = true;
+            }
+            document.querySelectorAll('.menu__item--has-dropdown').forEach((element) => {
+                element.classList.remove("menu__item--open");
+            });
+            if(open) { 
+                event.currentTarget.classList.remove("menu__item--open"); 
+                return;
+            }
+            event.currentTarget.classList.add("menu__item--open"); 
+            
+        })
+    })
+} 
+
+const hamburger = document.getElementById('hamburger')
+
+if(hamburger) {
+    hamburger.addEventListener('click', (event) => {
+        hamburger.firstElementChild.classList.toggle('is-active')
+        var menu = document.getElementById('hamburger-menu')
+            menu.classList.toggle("menu--open");
+    });
+} 
+
