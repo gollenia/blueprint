42b5b289b1b6ceaa2067d935799397f152723999
Thomas Gollenia <thomas@gollenia.at>
2021-12-01 17:08:49 +0100

    removed tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,10 @@
 const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
+
 
 //mix.postCss('./assets/src/css/style.css', './assets/dist/style.css', [
 //	require("tailwindcss"),
 //]);
 
-mix.postCss('./assets/src/css/admin.css', './assets/dist/admin.css');
 
 mix.js('./assets/src/js/app.js', './assets/dist/app.js');
 
