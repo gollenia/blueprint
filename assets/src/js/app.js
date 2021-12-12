93cf3e15950c1fe0a6aa6541870eaecb9cfaa29a
Thomas Gollenia <thomas@gollenia.at>
2021-12-12 16:37:30 +0100

    now module based

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,18 +1,6 @@
-import validateInput from "./validity.js";
-import  './booking.js';
-import  './menu.js';
+import validateInput from './validity.js';
+import { menuDrawer, menuDropdown } from './menu.js';
 
-
-
-document.addEventListener("focusout", validateInput);
-document.addEventListener("change", validateInput);
-
-
-document.addEventListener('click', (event) => {
-    document.querySelectorAll('.menu__item--has-dropdown').forEach((element) => {
-        element.classList.remove("menu__item--open");
-    });
-})
-
-
-
+validateInput();
+menuDropdown(1024);
+menuDrawer('hamburger');
