d4c1c226e6cdfbfeeeee66ce06b08df2b8505083
Thomas Gollenia <thomas@gollenia.at>
2021-12-12 16:37:04 +0100

    recoded

--- assets/src/js/validity.js
+++ assets/src/js/validity.js
@@ -1,26 +1,42 @@
-export default function validateInput (event) {
-    var errorMessage = event.target.parentElement.querySelector(".error-message");
-    if(errorMessage != undefined) {
-        errorMessage.remove();
-    }
-    if(!event.target.required) {
-        return;
-    }
-    if(event.target.validity.valid) {
-        event.target.parentElement.classList.remove("error");
-        return;
-    }
+/**
+ * This function extends the browsers own validity system. It removes the default error
+ *
+ * @param {Object} args
+ */
+const validateInput = (args) => {
+	const conf = {
+		events: ['focusout', 'change'],
+		errorClass: 'error',
+		...args,
+	};
 
-    event.target.parentElement.classList.add("error");
+	const addErrorListener = (event) => {
+		if (!event.target.required) return;
+		const errorMessage =
+			event.target.parentElement.querySelector('.error-message');
 
-    let span = document.createElement("span");
-    span.classList.add("error-message");
-    
-    span.innerText = event.target.validationMessage;
-    if(event.target.getAttribute("data-text-error")) {
-        span.innerText = event.target.getAttribute("data-text-error");
-    }
-    
-    event.target.parentNode.append(span);
-    return;
-}
+		if (errorMessage !== null) errorMessage.remove();
+
+		if (event.target.validity.valid) {
+			event.target.parentElement.classList.remove(conf.errorClass);
+			return;
+		}
+
+		event.target.parentElement.classList.add('error');
+
+		const message =
+			event.target.getAttribute('data-text-error') ??
+			event.target.validationMessage;
+
+		const span = document.createElement('span');
+		span.innerHTML = message;
+		span.classList.add('error-message');
+		event.target.parentNode.append(span);
+	};
+
+	for (event of conf.events) {
+		document.addEventListener(event, addErrorListener);
+	}
+};
+
+export default validateInput;
