2dfe78c033a8445f73c4e6e68ec4d586337a84a0
Thomas Gollenia <thomas@gollenia.at>
2021-12-14 15:20:02 +0100

    added Linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,9 @@ module.exports = {
 	extends: ['plugin:@wordpress/eslint-plugin/recommended'],
 	parserOptions: {
 		ecmaVersion: 12,
+		ecmaFeatures: {
+			jsx: true,
+		},
 		requireConfigFile: false,
 		sourceType: 'module',
 	},
