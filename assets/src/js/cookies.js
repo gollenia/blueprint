6d16d3d87b61034615023c310ab7dd9e7e3dcf23
Thomas Gollenia <thomas@gollenia.at>
2022-02-11 13:16:24 +0100

    rewritten for multiple cookie consent

--- assets/src/js/cookies.js
+++ assets/src/js/cookies.js
@@ -1,19 +1,43 @@
 const cookies = () => {
-	const sendConsentRequest = () => {
-		fetch('/wp-admin/admin-ajax.php?action=set_consent').then(
-			(response) => {
+	const sendConsentRequest = (all = false) => {
+		const args = all ? '1' : '0';
+		fetch('/wp-admin/admin-ajax.php?action=set_consent&all=' + args).then((response) => {
+				if (all) {
+					location.reload(true);
+				}
 			}
 		);
 	};
 
 	window.addEventListener('DOMContentLoaded', () => {
+		const consentBox = document.getElementById("allCookiesCheck");
+
 		const okClick = document.getElementById('consentPrivacy');
 		if (!okClick) return;
 		okClick.addEventListener('click', () => {
-			sendConsentRequest();
+			sendConsentRequest(consentBox.checked);
 			document
 				.getElementById('consentDialog')
 				.classList.remove('modal--open');
+		});
+
+		const allClick = document.getElementById('consentAll');
+		if (!allClick) return;
+		allClick.addEventListener('click', () => {
+			consentBox.checked = true;
+			sendConsentRequest(consentBox.checked);
+			document
+				.getElementById('consentDialog')
+				.classList.remove('modal--open');
+		});
+
+		const openDialog = document.getElementById('openCookiesDialog');
+		console.log(openDialog);
+		if (!openDialog) return;
+		openDialog.addEventListener('click', () => {
+			document
+				.getElementById('consentDialog')
+				.classList.add('modal--open');
 		});
 	});
 };
