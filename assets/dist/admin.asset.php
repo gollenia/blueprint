b05a4c01a099bbddf9c72ee155e16b4022560b3c
Thomas Gollenia <thomas@gollenia.at>
2022-02-17 09:49:48 +0100

    new build

--- assets/dist/admin.asset.php
+++ assets/dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '036c30ddef08e1bcee1ff27a345b4b19');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '392fc1ce1050e8ffa6138849b6c80f85');
