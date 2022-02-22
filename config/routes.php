ed1fd3f414d8f967613243587de12e29e0b0fc32
Thomas Gollenia <thomas@gollenia.at>
2022-02-22 13:45:02 +0100

    Event removerd

--- config/routes.php
+++ config/routes.php
@@ -16,13 +16,9 @@
  */
 
 return [
-    "Error" => is_404(),                      // Seite existiert nicht
+    "Error" => is_404(),                      // Seite existiert nicht - könnte in den normalen Controller übergehen
 
-    "Search" => !empty($_GET['ctx_search']),
-
-    //"Event" => is_singular('event'),        // Seite ist ein einzelnes Event
-
-    "Events" => get_option("dbem_cp_events_slug") ? is_page(get_option("dbem_cp_events_slug")) : false,          // Seite beinhaltet eine Eventliste
+    "Events" => get_option("dbem_cp_events_slug") ? is_page(get_option("dbem_cp_events_slug")) : false,          // Wird ausgegliedert
 
     "Post" => is_single(),
 
@@ -30,7 +26,5 @@ return [
 
     //"Frontpage" => is_front_page(),                    // Es handelt sich um eine normale Seite
 
-    "Page" => is_page(),                    // Es handelt sich um eine normale Seite
-
-    "Page" => true                          // Fallback
+    "Page" => true                          // Default
 ];
